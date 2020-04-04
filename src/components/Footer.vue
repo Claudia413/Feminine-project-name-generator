35f00527f0b30e22a2656a76becd84db6da2cbda
Claudia <cmjengelsman@gmail.com>
2020-04-05 11:24:29 +1300

    Add favicon and credit for Fontawesome icon use

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <p>Made by <a href="https://www.claudiaengelsman.com/" target="_blank" rel="noopener" >Claudia Engelsman</a> with <a href="https://vuejs.org/" target="_blank" rel="noopener"><i class="fab fa-vuejs"></i></a>, stored on <a href="https://github.com/Claudia413/Feminine-project-name-generator" target="_blank" rel="noopener"><i class="fab fa-github"></i></a> and hosted by <a href="https://www.netlify.com/" target="_blank" rel="noopener">Netlify</a>.</p>
+        <p>Made by <a href="https://www.claudiaengelsman.com/" target="_blank" rel="noopener" >Claudia Engelsman</a> with <a href="https://vuejs.org/" target="_blank" rel="noopener"><i class="fab fa-vuejs"></i></a> and <a href="https://fontawesome.com/" target="_blank" rel="noopener"><i class="fab fa-font-awesome"></i></a>, stored on <a href="https://github.com/Claudia413/Feminine-project-name-generator" target="_blank" rel="noopener"><i class="fab fa-github"></i></a>, and hosted by <a href="https://www.netlify.com/" target="_blank" rel="noopener">Netlify</a>.</p>
     </footer>
 </template>
 
